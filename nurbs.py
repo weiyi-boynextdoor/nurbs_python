5611f6d36bfa98012b00f1bfc08a5d6e5b330721
weiyiherrington <weiyiweishao@163.com>
2023-10-15 10:41:50 +0800

    update

--- nurbs.py
+++ nurbs.py
@@ -35,6 +35,12 @@ class Nurbs(object):
             for i in range(end -start + 1):
                 knot_index = start + i
                 new_basis = 0.0
+
+                if self.knot_vector[knot_index + i_order - 1] - self.knot_vector[knot_index] == 0:
+                    assert(buffer[i] == 0)
+                if  self.knot_vector[knot_index + i_order] - self.knot_vector[knot_index + 1] == 0:
+                    assert(buffer[i+1] == 0)
+                
                 if buffer[i] != 0.0:
                     new_basis += (t - self.knot_vector[knot_index]) * buffer[i] / (self.knot_vector[knot_index + i_order - 1] - self.knot_vector[knot_index])
                 if buffer[i+1] != 0.0:
@@ -79,22 +85,23 @@ class Nurbs(object):
         self.knot_vector = copy.deepcopy(knot_vector)
 
 if __name__ == "__main__":
-    n_points = 6
-    order = 3
+    n_points = 4
+    order = 4
 
     nurbs = Nurbs(n_points, order)
-    nurbs.set_template(False, False)
-    # nurbs.set_knot_vector([0,1,2,3.8,4,5])
+    nurbs.set_template(True, False)
+    # nurbs.set_knot_vector([0,1,2,3.5,4,5,6,7])
     
     overall_t_range = nurbs.get_overall_t_range()
     valid_t_range = nurbs.get_valid_t_range()
 
     t_range = overall_t_range
 
-    samples = 101
+    samples = 401
     step = float(t_range[1] - t_range[0]) / (samples - 1)
 
     x = [t_range[0] + step * i for i in range(samples)]
+    x[-1] = t_range[1]
     
     y_s = []
     for i in range(n_points):
@@ -102,15 +109,16 @@ if __name__ == "__main__":
 
     for i_x in range(len(x)):
         t = x[i_x]
-        start_and_valuess = nurbs.get_basis_value(t)
-        start = start_and_valuess["start"]
-        values = start_and_valuess["values"]
+        start_and_values = nurbs.get_basis_value(t)
+        start = start_and_values["start"]
+        values = start_and_values["values"]
         for i_v in range(len(values)):
             if start + i_v >= n_points:
                 continue
             y_s[start + i_v][i_x] = values[i_v]
-    
+
+    plt.figure(figsize=(12,6))
     for i in range(n_points):
         plt.plot(x, y_s[i], label="B%d"%i)
-    plt.legend()
+    plt.legend()    
     plt.show()
